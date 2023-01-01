e9b03437bbf9308c6d497b0b5987abfd6aafeddd
Piotr Jura <jura.piotrek@gmail.com>
2023-01-01 18:57:31 +0100

    010-008-exception-filters-gql

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { EntityNotFoundErrorFilter } from './entity-not-found-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -10,6 +11,7 @@ async function bootstrap() {
         : ['error', 'warn'],
   });
   app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalFilters(new EntityNotFoundErrorFilter());
   await app.listen(3000);
 }
 bootstrap();
