b3b9c7b223f4f7c91c067b120e693e91c30dc6d5
Remy Tumbar <remy@coherentdataengines.com>
2023-02-25 14:39:20 -0500

    added logging to main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger:
+      process.env.NODE_ENV === 'dev'
+        ? ['log', 'debug', 'error', 'verbose', 'warn']
+        : ['error', 'warn'],
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
